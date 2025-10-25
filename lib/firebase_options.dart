aabf7055cbb1dd79015f630271ccf58ed12f42e1
azref <azreftravaux@gmail.com>
2025-10-25 01:33:45 +0100

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,5 +1,12 @@
+// @dart=2.19
+// The file supports the following Flutter hooks:
+//   * `flutterfire configure`
+//   * `flutterfire gen-ios-config`
+//   * `flutterfire gen-android-config`
+
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -13,7 +20,8 @@ class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
       throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - ',
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
       );
     }
     switch (defaultTargetPlatform) {
@@ -23,15 +31,18 @@ class DefaultFirebaseOptions {
         return ios;
       case TargetPlatform.macOS:
         throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - ',
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
         );
       case TargetPlatform.windows:
         throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - ',
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
         );
       case TargetPlatform.linux:
         throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for linux - ',
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
         );
       default:
         throw UnsupportedError(
@@ -45,7 +56,7 @@ class DefaultFirebaseOptions {
     appId: '1:745606809547:android:3c2574edfff1748928984e',
     messagingSenderId: '745606809547',
     projectId: 'elsane3-95c91',
-    storageBucket: 'elsane3-95c91.firebasestorage.app',
+    storageBucket: 'elsane3-95c91.appspot.com',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
@@ -58,5 +69,3 @@ class DefaultFirebaseOptions {
     iosBundleId: 'com.elsane3.app',
   );
 }
-
-
